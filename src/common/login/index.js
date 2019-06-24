794aa521eca0b9d569eb4d4eb6a898b02e6e6c49
john ornelas <johnnyoh1988@gmail.com>
2019-06-24 12:45:05 -0700

    feat(LoginComponent) scaffold dir structure for login component

---
+++ src/common/login/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class LoginComponent extends Component {
+  render() {
+    return (
+      <View>
+        <Text>this is the login component</Text>
+      </View>
+    );
+  }
+}
+
+LoginComponent.propTypes = {
+
+};
+
+export default LoginComponent;
