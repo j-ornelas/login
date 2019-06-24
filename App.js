422db025c33a46c3386e4e440ae2f61bb4f3735a
john ornelas <johnnyoh1988@gmail.com>
2019-06-24 12:55:06 -0700

    feat(App.js) add required props to reusable Login Component

--- App.js
+++ App.js
@@ -3,10 +3,31 @@ import { StyleSheet, Text, View } from 'react-native';
 import { LoginComponent } from './src/common';
 
 export default class App extends React.Component {
+  state = {
+    email: '',
+    password: ''
+  }
+
+  exampleOnChange(prop, value) {
+    this.setState({ [prop]: value });
+  }
+
+  exampleOnSubmit() {
+    console.log(`
+      The current email is ${this.state.email}.
+      The current password is ${this.state.password}.
+    `);
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <LoginComponent />
+        <LoginComponent
+          email={this.state.email}
+          password={this.state.password}
+          onChange={(prop, val) => this.exampleOnChange(prop, val)}
+          onSubmit={this.exampleOnSubmit}
+        />
       </View>
     );
   }
