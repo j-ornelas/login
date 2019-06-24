e1c8b3f8601bca57f1046669d9215dae1e43e481
john ornelas <johnnyoh1988@gmail.com>
2019-06-24 12:57:13 -0700

    refactor(LoginComponent) change Login from Class Component to functional component

--- src/common/login/index.js
+++ src/common/login/index.js
@@ -1,15 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { View, Text } from 'react-native';
 import PropTypes from 'prop-types';
 
-class LoginComponent extends Component {
-  render() {
-    return (
-      <View>
-        <Text>this is the login component</Text>
-      </View>
-    );
-  }
+const LoginComponent = (props) => {
+  return (
+    <View>
+      <Text>this is the login component</Text>
+    </View>
+  );
 }
 
 LoginComponent.propTypes = {
